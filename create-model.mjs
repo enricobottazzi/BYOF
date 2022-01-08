69c175c17426064c6d39fa9c62853198494f12a7
enricobottazzi <85900164+enricobottazzi@users.noreply.github.com>
2022-01-08 02:03:53 +0100

    Update create-model.mjs

--- create-model.mjs
+++ create-model.mjs
@@ -1,31 +1,61 @@
-import { Ed25519Provider } from 'key-did-provider-ed25519'
-import { randomBytes } from '@stablelib/random'
+// Use this to create a new Data Model
+
+import { writeFile } from 'node:fs/promises'
 import { CeramicClient } from '@ceramicnetwork/http-client'
-import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
+import { ModelManager } from '@glazed/devtools'
 import { DID } from 'dids'
+import { Ed25519Provider } from 'key-did-provider-ed25519'
+import { getResolver } from 'key-did-resolver'
+import { fromString } from 'uint8arrays'
 
 async function main () {
 
-const API_URL = 'https://localhost:7007'
-const ceramic = new CeramicClient(API_URL)
+const DID_KEY="<>"
+// The key must be provided as an environment variable
+const key = fromString(DID_KEY, 'base16')
+// Create and authenticate the DID
+const did = new DID({
+  provider: new Ed25519Provider(key),
+  resolver: getResolver(),
+})
 
-const resolver = {
-  ...ThreeIdResolver.getResolver(ceramic),
-}
+await did.authenticate()
 
-const did = new DID({ resolver })
+// Connect to the local Ceramic node
+const ceramic = new CeramicClient('http://localhost:7007')
 ceramic.did = did
 
-const seed = randomBytes(32)
+// Create a manager for the model
+const manager = new ModelManager(ceramic)
 
-const provider = new Ed25519Provider(seed)
+const BYOFID = await manager.createSchema('BYOFGraph', {
+  $schema: "http://json-schema.org/draft-07/schema#",
+  title: "BYOFV0.1",
+  type: "object",
+  properties: {
+    address: {
+      type: "string"
+    },
+    followingList: {
+      type: "array",
+      items:{
+        type: "string"
+      }
+    }
+  }
+});
 
-ceramic.did.setProvider(provider)
+  await manager.createDefinition('BYOFGRPH', {
+    name: 'BYOF Graph',
+    description: 'Own your social graph and bring it around',
+    schema: manager.getSchemaURL(BYOFID),
+  })
+  
+  const model = await manager.toPublished()
 
-const data = await ceramic.did.authenticate()
-
-console.log(data)
+  await writeFile('./BYOFGRPH.json', JSON.stringify(model))
 
 }
 
 main()
+
